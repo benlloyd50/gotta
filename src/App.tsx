78c6341e9b8bf3acf78a9dec934a16741ceb9c96
Benjamin Lloyd <benji6090@gmail.com>
2023-09-23 00:53:36 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// TODO: subtitles on cards
+export function TodoCard() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li className='todo-card'>
+      <div className='todo-card__label'>Long Silly Task</div>
+      <button className='todo-card__check todo-card--groovy'>✔️</button>
+      <button className='todo-card__delete todo-card--groovy'>🗑️</button>
+    </li>
   );
+}
+
+export function TodoList() {
+  // TODO1: implement local storage saving of tasks
+  // TODO2: implement mongo db saving of tasks
+  return (
+    <ol className='todo-list'>
+      <TodoCard />
+      <TodoCard />
+      <TodoCard />
+      <AddTodoCard />
+    </ol>
+  );
+}
+
+export function AddTodoCard() {
+  return (
+    <li className='add-todo'>
+      <input
+        type='text'
+        className='add-todo__small-input'
+        placeholder='I gotta do...'
+        name='taskname'
+      ></input>
+      <input type='submit' className='todo-card--groovy' value='➕'></input>
+    </li>
+  );
+}
+
+function App() {
+  return <TodoList />;
 }
 
 export default App;
