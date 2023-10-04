4e4cffc1fe525887b17bdc6e0b858f428fb38ea2
Benjamin Lloyd <benji6090@gmail.com>
2023-10-04 13:47:24 -0400

    add connection psql with dummy values

--- gotta_backend/src/main.rs
+++ gotta_backend/src/main.rs
@@ -5,17 +5,28 @@ use axum::{
 };
 
 use serde::{Serialize, Deserialize};
+use sqlx::postgres::{PgPoolOptions, PgRow};
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/task", get(get_task).post(post_task).put(put_task))
-        .route("/tasks", get(get_tasks));
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://demouser:123@localhost:5432/gottadb")
+        .await?;
+    
+    let rows: Vec<Task> = sqlx::query_as!(Task, r"SELECT * FROM task").fetch_all(&pool).await?;
+    // let all_tasks = rows.iter().map(|r| r.)
+    println!("{:?}", rows);
+    Ok(())
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // let app = Router::new()
+    //     .route("/task", get(get_task).post(post_task).put(put_task))
+    //     .route("/tasks", get(get_tasks));
+    //
+    // axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
 }
 
 //TODO: figure out database setup for this and the database itself
@@ -24,9 +35,9 @@ async fn main() {
 // Get, Post, Put Task
 // Get Tasks
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct Task {
-    // owner_id: usize,
+    id: i64,
     name: String,
     is_completed: bool,
 }
@@ -38,7 +49,7 @@ struct CreateTask {
 }
 
 async fn get_task() -> Json<Task> {
-    let fake_task = Task { name: "Homework".to_string(), is_completed: false};
+    let fake_task = Task { id: 99, name: "Homework".to_string(), is_completed: false};
     Json(fake_task)
 }
 
