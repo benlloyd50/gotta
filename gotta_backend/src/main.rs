4559782d006a3dbf5e1f47697432fb7925f1ec6c
Benjamin Lloyd <benji6090@gmail.com>
2023-10-18 15:18:29 -0400

    complete backend as per current spec described in main.rs

--- gotta_backend/src/main.rs
+++ gotta_backend/src/main.rs
@@ -1,6 +1,10 @@
-use axum::routing::post;
+use axum::{extract::Path, routing::post};
 #[allow(unused)]
-use axum::{extract::State, routing::get, Json, Router};
+use axum::{
+    extract::State,
+    routing::{get, put},
+    Json, Router,
+};
 
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
@@ -13,7 +17,8 @@ async fn main() -> Result<(), sqlx::Error> {
         .await?;
 
     let app = Router::new()
-        .route("/task", get(get_task).post(create_task).put(put_task))
+        .route("/task", post(create_task))
+        .route("/task/:id", get(get_task).put(update_task))
         .route("/tasks", get(get_tasks))
         .with_state(db);
 
@@ -25,11 +30,11 @@ async fn main() -> Result<(), sqlx::Error> {
     Ok(())
 }
 
-//TODO: figure out database setup for this and the database itself
-
 // Todo Backend
-// Get, Post, Put Task
-// Get Tasks
+// GET /task/:id -> return a single task of id or error
+// PUT /task/:id -> update a single task and return the task or error
+// GET /tasks -> return all tasks or error
+// POST /task -> create a new task and return the id or error
 
 #[derive(Serialize, Debug)]
 struct Task {
@@ -44,48 +49,87 @@ struct CreateTask {
     is_completed: bool,
 }
 
-async fn get_task() -> Json<Task> {
-    let fake_task = Task {
-        id: 99,
-        name: "Homework".to_string(),
-        is_completed: false,
-    };
-    Json(fake_task)
+#[derive(Deserialize, Debug)]
+struct UpdateTask {
+    name: String,
+    is_completed: bool,
 }
 
-async fn complete_task() {}
+#[derive(Serialize, Debug)]
+struct TaskID {
+    id: i64,
+}
+
+impl TaskID {
+    fn new(id: i64) -> Self {
+        Self { id }
+    }
+}
+
+async fn get_task(State(pool): State<PgPool>, Path(id): Path<i64>) -> Result<Json<Task>, String> {
+    match sqlx::query_as!(Task, "SELECT * FROM task WHERE id = $1", id)
+        .fetch_one(&pool)
+        .await
+    {
+        Ok(tasks) => Ok(Json(tasks)),
+        Err(_) => return Err("Cannot retrieve tasks from database".to_string()),
+    }
+}
+
+// For the simplicity of this project all tasks belong to one user, the person locally running the app
+async fn get_tasks(State(pool): State<PgPool>) -> Result<Json<Vec<Task>>, String> {
+    println!("GETALLTASKS");
+    match sqlx::query_as!(Task, "SELECT * FROM task")
+        .fetch_all(&pool)
+        .await
+    {
+        Ok(tasks) => Ok(Json(tasks)),
+        Err(_) => return Err("Cannot retrieve tasks from database".to_string()),
+    }
+}
 
 async fn create_task(
     State(pool): State<PgPool>,
     Json(payload): Json<CreateTask>,
-) -> Result<Json<i64>, String> {
+) -> Result<Json<TaskID>, String> {
     println!("CREATETASK: recieved {:?}", payload);
-    let new_task = match sqlx::query!(
-        r#"
+    match sqlx::query!(
+        "
         INSERT INTO task (name, is_completed)
         VALUES ( $1, $2)
         RETURNING id
-        "#,
+        ",
         payload.name,
         payload.is_completed
     )
     .fetch_one(&pool)
     .await
     {
-        Ok(task) => task,
-        Err(_) => {
-            return Err("Cannot create task".to_string())
-        }
-    };
-
-    Ok(Json(new_task.id))
+        Ok(task) => Ok(Json(TaskID::new(task.id))),
+        Err(_) => return Err("Cannot create task".to_string()),
+    }
 }
-async fn put_task() {}
 
-async fn get_tasks() {
-    // let rows: Vec<Task> = sqlx::query_as!(Task, r"SELECT * FROM task")
-    //     .fetch_all(&db)
-    //     .await?;
-    // let all_tasks = rows.iter().map(|r| r.)
-    // println!("{:?}", rows);
+async fn update_task(
+    State(pool): State<PgPool>,
+    Path(id): Path<i64>,
+    Json(payload): Json<UpdateTask>,
+) -> Result<Json<Task>, String> {
+    println!("UPDATETASK: recieved {:?}", payload);
+    match sqlx::query_as!(
+        Task,
+        "UPDATE task 
+                 SET name = $1, is_completed = $2
+                 WHERE id = $3
+                 RETURNING id, name, is_completed",
+        payload.name,
+        payload.is_completed,
+        id
+    )
+    .fetch_one(&pool)
+    .await
+    {
+        Ok(updated_task) => Ok(Json(updated_task)),
+        Err(_) => Err("Cannot update task".to_string()),
+    }
 }
